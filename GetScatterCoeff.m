8909d753284890e0373c655f0dfbcd209aa8dc47
Ma Xihan <31639301+MXHsj@users.noreply.github.com>
2021-10-01 01:03:56 -0400

    improved scatter-coeff calculation

--- GetScatterCoeff.m
+++ GetScatterCoeff.m
@@ -15,13 +15,29 @@ end
 mu_s = zeros(1,size(BScan,2)); a = zeros(1,size(BScan,2));
 
 % ====================== fast ver. =======================
+for col = 1:size(BScan,2)
+    AScan = BScan(:,col);
+    [peak_val, peak_ind] = max(AScan);
+%     if peak_val
+    % solve for Y = xB
+    x = [ones(length(AScan(peak_ind:end)),1), (1:length(AScan(peak_ind:end)))'];
+    if length(x) > 1 && peak_val > intThresh
+        f = x\double(AScan(peak_ind:end));
+        mu_s(col) = f(end)/-8.7; 
+        a(col) = 10^(f(1)/10);
+    else
+        mu_s(col) = nan; 
+        a(col) = nan;
+    end
+end
+
+% ====================== slower ver. =======================
 % for col = 1:size(BScan,2)
 %     AScan_filt = BScan(:,col);
 %     [peak_val, peak_ind] = max(AScan_filt);
-%     % solve for Y = xB
-%     x = [ones(length(AScan_filt(peak_ind:end)),1), (1:length(AScan_filt(peak_ind:end)))'];
+%     x = (1:length(AScan_filt(peak_ind:end)))';
 %     if length(x) > 1 && peak_val > intThresh
-%         f = x\double(AScan_filt(peak_ind:end));
+%         f = polyfit(x,double(AScan_filt(peak_ind:end)),1);
 %         mu_s(col) = f(1)/-8.7; 
 %         a(col) = 10^(f(end)/10);
 %     else
@@ -31,38 +47,29 @@ mu_s = zeros(1,size(BScan,2)); a = zeros(1,size(BScan,2));
 % %     mu_s = filloutliers(mu_s,'previous');
 % end
 
-% ====================== slower ver. =======================
-for col = 1:size(BScan,2)
-    AScan_filt = BScan(:,col);
-    [peak_val, peak_ind] = max(AScan_filt);
-    x = (1:length(AScan_filt(peak_ind:end)))';
-    if length(x) > 1 && peak_val > intThresh
-        f = polyfit(x,double(AScan_filt(peak_ind:end)),1);
-        mu_s(col) = f(1)/-8.7; 
-        a(col) = 10^(f(end)/10);
-    else
-        mu_s(col) = nan; 
-        a(col) = nan;
-    end
-%     mu_s = filloutliers(mu_s,'previous');
-end
-
 % toc
 % visualization
 if isVisualize
-    figure('Position',[500,95,850,400])
+    figure('Position',[600,400,1.2*size(BScan,2),1.2*size(BScan,1)/2])
     subplot(1,2,1)
-    imagesc(BScan)
-    title('raw B-scan')
-
+    yyaxis left; 
+    imagesc(BScan); 
+    ylabel('image height [row]')
+    yyaxis right; 
+    plot(1:length(mu_s),mu_s,'o','MarkerSize',3.5); 
+    ylabel('scattering coefficient \mu_s [pix^{-1}]')
+    xlabel('image width [col]')
+    title('BScan')
+    
     subplot(1,2,2)
-    plot((AScan_filt), 1:length(AScan_filt))
-    hold on
-    [~, peak_ind] = max(AScan_filt);
-    z = (peak_ind:length(AScan_filt));
-    plot(10*log10(a(end))-(8.7*mu_s(end)*(z-peak_ind)),z,'--','LineWidth',2);
-    ylabel('pixel'); xlabel('intensity')
-    title('A-scan of last column')
-    legend('intensity','fitted intensity')
+    AScan2plot = 30;
+    plot((BScan(:,AScan2plot)), 1:length(BScan(:,AScan2plot))); hold on
+    [~, peak_ind] = max(BScan(:,AScan2plot));
+    z = (peak_ind:length(BScan(:,AScan2plot)));
+    plot(10*log10(a(AScan2plot))-(8.7*mu_s(AScan2plot)*(z-peak_ind)),z,'--','LineWidth',2);
+    ylabel('image height [row]'); 
+    xlabel('intensity [dB]')
+    legend('intensity',['linear decay {\mu_s = }',num2str(mu_s(AScan2plot))],'Location','northwest')
     axis('tight','ij')
+    title(['AScan at ','col = ',num2str(AScan2plot)]);
 end
--- OCT3D_scatter_coeff.m
+++ OCT3D_scatter_coeff.m
@@ -6,17 +6,16 @@
 clc; clear; close all
 isGenVid = false;
 % load BScan & pose data
-data2load = 23:25;
+data2load = 24;
 [data, data_sizes] = DataManagerOCT(data2load); 
 
 %% extract first peak from AScan
 probe = ProbeConfigOCT(); % get OCT probe configuration
 enCalibTune = true;
-limRange = true;
 T_flange_probe_new = CompCalibErr(probe.T_flange_probe);
 
-pc_x = []; pc_y = []; pc_z = []; 
-scatter_coeff = [];     % single scattering model
+pc_x = []; pc_y = []; 
+scatter_coeff = []; % single scattering model
 
 dwnSmpInterv = 0.00;
 imgFiltThresh = 48;
@@ -42,13 +41,12 @@ for item = 1:size(data.OCT,3)
             T_base_flange = T/probe.T_flange_probe; % T*inv(probe.T_flange_probe)
             T = T_base_flange * T_flange_probe_new;
         end
-        [xglobal, yglobal, zglobal] = TransformPoints(T,xlocal,ylocal,zlocal);
+        [xglobal, yglobal, ~] = TransformPoints(T,xlocal,ylocal,zlocal);
         
         % downsample
         if dwnSmpInterv > 0
             xglobal = downsample(xglobal,ceil(dwnSmpInterv*length(xglobal)));
             yglobal = downsample(yglobal,ceil(dwnSmpInterv*length(yglobal)));
-            zglobal = downsample(zglobal,ceil(dwnSmpInterv*length(zglobal)));
             sc = downsample(sc, ceil(dwnSmpInterv*length(sc)));
         end
         
@@ -59,26 +57,27 @@ for item = 1:size(data.OCT,3)
         end
         pc_x = cat(2, pc_x, xglobal);
         pc_y = cat(2, pc_y, yglobal);
-        pc_z = cat(2, pc_z, zglobal);
         scatter_coeff = cat(2, scatter_coeff, sc(~isnan(sc)));
     end 
 end
-pc_x = single(pc_x); pc_y = single(pc_y); pc_z = single(pc_z);
+pc_x = single(pc_x); pc_y = single(pc_y); 
+scatter_coeff = single(scatter_coeff);
 fprintf('processing data takes %f sec \n', toc);
 
 %% limit scattering coeff value range
-if limRange
-    lowBound = 0; upBound = 6.5e-3; % 0.015; 
-    outlier_ind = find(scatter_coeff < lowBound | scatter_coeff > upBound);
-    scatter_coeff(outlier_ind) = nan;
-end
+lowBound = 0; % median(scatter_coeff) - std(scatter_coeff);
+upBound = mean(scatter_coeff) + 0.1*std(scatter_coeff);
+outlier_ind = find(scatter_coeff < lowBound | scatter_coeff > upBound);
+scatter_coeff(outlier_ind) = nan;
 
 %% visualize 2D scattering coefficient map
 figure('Position',[500,120,1000,600])
-scatter(pc_x.*1e3,pc_y.*1e3,repmat(1,1,length(pc_x)),scatter_coeff,'filled')
+scatter(pc_x*1e3,pc_y*1e3,ones(1,length(pc_x)),scatter_coeff,'filled')
 colormap(gca,'parula') % jet
-cb = colorbar('Ticks',linspace(min(scatter_coeff),max(scatter_coeff),5));
+cb = colorbar('Ticks',linspace(min(scatter_coeff),max(scatter_coeff),4));
 cb.Label.String = 'scattering coefficient '; cb.Label.FontSize = 14;
+xlim([min(pc_x*1e3),max(pc_x*1e3)]);
+ylim([min(pc_y*1e3),max(pc_y*1e3)]);
 xlabel('x [mm]'); ylabel('y [mm]');
-axis equal tight 
+axis equal
 % axis off
