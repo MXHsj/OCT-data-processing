04ec3b4a7f37b603e583e6e8baad07d563240804
Ma Xihan <xma4@wpi.edu>
2021-12-04 14:11:07 -0500

    Update franka_OCT_bridge.m

--- franka_OCT_bridge.m
+++ franka_OCT_bridge.m
@@ -9,25 +9,24 @@ clc; clear; close all
 
 %% ----------------- ROS network -----------------
 rosshutdown
+ros_master_uri = 'http://130.215.209.225:11311';
 % [~, local_ip] = system('ipconfig');
-setenv('ROS_MASTER_URI','http://130.215.121.229:11311') % ip of robot desktop
-setenv('ROS_IP','130.215.192.178')   % ip of this machine
+local_ip = '130.215.192.168';
+setenv('ROS_MASTER_URI',ros_master_uri) % ip of robot desktop
+setenv('ROS_IP',local_ip)   % ip of this machine
 rosinit
 
 % ----------------- receive message from robot -----------------
-OCT_clk_ctrl_sub = rossubscriber('OCT_clk_ctrl', 'std_msgs/Int8', @OCT_clk_ctrl_callback);
+OCT_scan_flag_sub = rossubscriber('OCT_scan_flag', 'std_msgs/Int8', @OCT_scan_flag_callback);
 franka_pos_sub = rossubscriber('franka_state_custom', 'std_msgs/Float64MultiArray');
-global OCT_clk_ctrl
 franka_pose = zeros(4,4);
-% last_franka_pose = zeros(4,4);
 
 % ----------------- send message to robot -----------------
-OCT_img_pub = rospublisher('OCT_img_fb', 'std_msgs/Float64MultiArray');
-OCT_img_msg = rosmessage(OCT_img_pub);
-% height, in-plane slope, out-of-plane slope
+OCT_response_pub = rospublisher('OCT_remote_response', 'std_msgs/Float64MultiArray');
+OCT_response_msg = rosmessage(OCT_response_pub);
 height = 1024; width = 1024;
-OCT_img_msg.Data = [height, 0.0, 0.0];
-last_surf_height = 0;
+isDataSaved = 0;
+OCT_response_msg.Data = [height, 0.0, isDataSaved];
 
 %% ----------------- initialize OCT -----------------
 [Dev, RawData, Data, Proc, Probe, ScanPattern] = LoadSpectralRadar();
@@ -35,18 +34,24 @@ last_surf_height = 0;
 % ---------------------------------------------------
 %% main loop
 % constant
+clear BScan_queue pose_queue
+sample_name = 'breast';
 freq = 20;      % max: 22; default: 20
 rate = rateControl(freq);
-OCT_clk_ctrl = 0;
+global OCT_scan_flag
+OCT_scan_flag = 0;
 isStartScan = false;                % robot start scanning flag
-queue_size = 2800;
-intensity_thresh = 56;
+queue_size = 2500;
+intensity_thresh = 55;              % threshold above which will be considered as tissue
 rms_err_thresh = 0.035*height;
 store_img_height = 700;
-data_count = 1;
+% counters
+global frm_count scan_count
+frm_count = 1; scan_count = 1;
 % pre-allocation
 BScan_bw = zeros(height,width,'logical');
 surf_row_ind = zeros(1,width);
+global BScan_queue pose_queue
 BScan_queue = zeros(store_img_height,width,queue_size,'uint8');   % use uint8 to save space
 pose_queue = zeros(4,4,queue_size,'double');
 
@@ -55,15 +60,17 @@ while true
     % ----------------- receive from robot ----------------
     franka_pose_msg = receive(franka_pos_sub);
     franka_pose = reshape([franka_pose_msg.Data],4,4)';
-
-    if OCT_clk_ctrl == 1 && isStartScan == false
+    if OCT_scan_flag == 1 && isStartScan == false
         isStartScan = true;
+        isDataSaved = 0;
         disp('robot start scanning')
     end
-    if OCT_clk_ctrl == 0 && isStartScan == true
+    if OCT_scan_flag == 0 && isStartScan == true
         isStartScan = false;
         disp('robot stop scanning')
-        break
+        save_data(sample_name);
+        isDataSaved=1; frm_count=1; scan_count = scan_count+1;
+        % break
     end
     % -----------------------------------------------------
     
@@ -88,48 +95,34 @@ while true
     
     % ----------------- record OCT & pose data ----------------
     if isStartScan
-        BScan_queue(:,:,data_count) = uint8(BScan(1:store_img_height,:));
-        pose_queue(:,:,data_count) = franka_pose;
-        if data_count >= queue_size
-            disp('reached maximum storage')
-            break
-        end
-        data_count = data_count + 1;
+        BScan_queue(:,:,frm_count) = uint8(BScan(1:store_img_height,:));
+        pose_queue(:,:,frm_count) = franka_pose;
+        frm_count = frm_count + 1;
+    end
+    if frm_count >= queue_size
+        disp('reached maximum storage')
+        break
     end
     % ---------------------------------------------------------
     
     % ----------------- send to robot ----------------
     if ~isempty(surf_height)
-        OCT_img_msg.Data(1) = 1-surf_height/height;    % surface height
+        OCT_response_msg.Data(1) = 1-surf_height/height;    % surface height
     else
-        OCT_img_msg.Data(1) = 0.0;
+        OCT_response_msg.Data(1) = 0.0;
     end
     if ~isempty(B) && rms_err < rms_err_thresh
-        OCT_img_msg.Data(2) = B(2);                % in-plane slope
+        OCT_response_msg.Data(2) = B(2);                % in-plane slope
     else
-        OCT_img_msg.Data(2) = 0.0;
+        OCT_response_msg.Data(2) = 0.0;
     end
-%     obj_slope = (last_franka_pose(3,4) - last_surf_height*0.01) - ...
-%                 (franka_pose(3,4) - surf_height*0.01);
-%     traj_slope = last_franka_pose(3,4) - franka_pose(3,4);
-%     fprintf('obj_slope: %f \t traj_slope: %f \n', obj_slope, traj_slope)
-%     OCT_img_msg.Data(3) = obj_slope - traj_slope;   % out-of-plane slope
-%     last_surf_height = surf_height;
-%     last_franka_pose = franka_pose;
+    OCT_response_msg.Data(3) = isDataSaved;
     % send message
-    send(OCT_img_pub, OCT_img_msg)
+    send(OCT_response_pub, OCT_response_msg)
     % ------------------------------------------------
     waitfor(rate);
-    fprintf('loop time: %f, isStartScan: %d\n', toc, isStartScan);
+%     fprintf('loop time: %f, isStartScan: %d\n', toc, isStartScan);
 end
-
-%% save data
-tic;
-BScan2save = BScan_queue(:,:,1:data_count);
-pose2save = pose_queue(:,:,1:data_count);
-save(['../data/',date,'_BScan{breast7}.mat'],'BScan2save')
-save(['../data/',date,'_franka_pose{breast7}.mat'],'pose2save')
-fprintf('save data took: %f sec\n', toc);
 
 %% finish
 UnloadSpectralRadar(Dev, RawData, Data, Proc, Probe, ScanPattern);
@@ -137,7 +130,17 @@ clear Dev RawData Data Proc Probe ScanPattern
 unloadlibrary SpectralRadar
 
 %% utilities
-function OCT_clk_ctrl_callback(~,message)
-global OCT_clk_ctrl
-OCT_clk_ctrl = [message.Data];
+function OCT_scan_flag_callback(~,message)
+global OCT_scan_flag
+OCT_scan_flag = [message.Data];
+end
+
+function save_data(name)
+global BScan_queue pose_queue frm_count scan_count
+tic;
+BScan2save = BScan_queue(:,:,1:frm_count);
+pose2save = pose_queue(:,:,1:frm_count);
+save(['../data/',date,'_BScan{',name,num2str(scan_count),'}.mat'],'BScan2save')
+save(['../data/',date,'_franka_pose{',name,num2str(scan_count),'}.mat'],'pose2save')
+fprintf('save data took: %f sec\n', toc);
 end
